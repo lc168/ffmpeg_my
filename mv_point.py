eb1ca4cf20f7b78633bcc251d7be2c77ed1cc82d
liucheng <liucheng@email>
2018-06-03 21:10:58 +0800

    还想解决，节点太多无法画图的问题

---
+++ mv_point.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2
+# -*- coding: utf-8 -*-
+#写一个py程序去掉graph.dot 重复的点
+
+print "liucheng"
+
+#读取graph.dot 到最后一行
+
+  #如果这一行包含 “xxx -> xxx ”  结构
+    #检查 不存在于map中
+        #添加到map
+        #写入新文件
+      
+         
+  #否则  写入新文件
+
